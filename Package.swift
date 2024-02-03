778c80a2ef3d5177965064f5ea5d74ef5c3984da
Nikolai Nobadi <nikolai.nobadi@gmail.com>
2024-02-03 15:41:39 -0800

    initial commit with basic functionality

--- Package.swift
+++ Package.swift
@@ -5,19 +5,36 @@ import PackageDescription
 
 let package = Package(
     name: "NnExecutableManager",
+    platforms: [
+        .macOS(.v10_14),
+    ], 
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "NnExecutableManager",
             targets: ["NnExecutableManager"]),
+        .executable(
+            name: "NnExecutableManagerExecutable",
+            targets: ["NnExecutableManagerExecutable"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/JohnSundell/Files", from: "4.0.0"),
+//        .package(url: "https://github.com/kareman/SwiftShell", from: "5.0.0"),
+//        .package(url: "https://github.com/nikolainobadi/SwiftPickerCLI", branch: "main"),
+        .package(url: "https://github.com/apple/swift-argument-parser.git", from: "0.3.0")
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "NnExecutableManager"),
-        .testTarget(
-            name: "NnExecutableManagerTests",
-            dependencies: ["NnExecutableManager"]),
+            name: "NnExecutableManager",
+            dependencies: [
+                "Files",
+//                "SwiftShell",
+//                "SwiftPickerCLI",
+                .product(name: "ArgumentParser", package: "swift-argument-parser")
+            ]
+        ),
+        .executableTarget(
+            name: "NnExecutableManagerExecutable",
+            dependencies: ["NnExecutableManager"]
+        )
     ]
 )
---
+++ Sources/NnExecutableManagerExecutable/main.swift
@@ -0,0 +1,53 @@
+//
+//  File.swift
+//  
+//
+//  Created by Nikolai Nobadi on 2/3/24.
+//
+
+import Files
+import ArgumentParser
+
+fileprivate let nnToolsPath = "/Users/nelix/nnTools"
+
+struct MainCommand: ParsableCommand {
+    static let configuration = CommandConfiguration(abstract: "Utility to manage and copy Swift project executables into nnTools directory.")
+    
+    @Option(name: [.customLong("exec"), .customShort("e")], help: "Specify 'debug' or 'release' to copy the corresponding executable.")
+    var buildConfiguration: String
+    
+    func validate() throws {
+        guard buildConfiguration == "debug" || buildConfiguration == "release" else {
+            throw ValidationError("Invalid build configuration: \(buildConfiguration). Use 'debug' or 'release'.")
+        }
+    }
+    
+    func run() throws {
+        let currentFolderName = Folder.current.name
+        let nnToolsFolder = try Folder(path: nnToolsPath)
+        let projectFolder = try nnToolsFolder.createSubfolderIfNeeded(withName: currentFolderName)
+        
+        guard let executableFile = try fetchExecutable(folderName: currentFolderName) else {
+            print("Executable not found. Make sure to build the project first.")
+            return
+        }
+        
+        try copyExecutable(file: executableFile, to: projectFolder)
+    }
+    
+    func fetchExecutable(folderName: String) throws -> File? {
+        let buildFolder = try Folder.current.subfolder(at: ".build/\(buildConfiguration)")
+    
+        return buildFolder.files.first(where: { $0.nameExcludingExtension.contains("\(folderName)") && $0.extension == nil })
+    }
+    
+    func copyExecutable(file: File, to projectFolder: Folder) throws {
+        if projectFolder.containsFile(named: file.name) {
+            try projectFolder.file(named: file.name).delete()
+        }
+        
+        try file.copy(to: projectFolder)
+        
+        print("Successfully copied \(buildConfiguration) executable to \(projectFolder.path)/\(file.name)")
+    }
+}
