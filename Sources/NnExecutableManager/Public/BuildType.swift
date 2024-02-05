e4ecf207eeca9cafc7f544609419409fb846b702
Nikolai Nobadi <nikolai.nobadi@gmail.com>
2024-02-04 17:00:07 -0800

    made it so that --exec isn't necessary to run exec file

--- Sources/NnExecutableManager/Public/BuildType.swift
+++ Sources/NnExecutableManager/Public/BuildType.swift
@@ -7,4 +7,10 @@
 
 public enum BuildType: String {
     case debug, release
+    
+    public init?(type: String?) {
+        guard let type = type, let instance = BuildType(rawValue: type) else { return nil }
+        
+        self = instance
+    }
 }
--- Sources/NnExecutableManagerExecutable/main.swift
+++ Sources/NnExecutableManagerExecutable/main.swift
@@ -12,19 +12,13 @@ struct MainCommand: ParsableCommand {
     static let configuration = CommandConfiguration(abstract: "Utility to manage and copy Swift project executables into nnTools directory.")
     
     @Option(name: [.customLong("exec"), .customShort("e")], help: "Specify 'debug' or 'release' to copy the corresponding executable.")
-    var buildConfiguration: String
+    var buildConfiguration: String?
     
     @Option(name: [.customLong("path"), .customShort("p")], help: "Optional path to the project directory.")
-        var path: String?
-    
-    func validate() throws {
-        guard buildConfiguration == "debug" || buildConfiguration == "release" else {
-            throw ValidationError("Invalid build configuration: \(buildConfiguration). Use 'debug' or 'release'.")
-        }
-    }
+    var path: String?
     
     func run() throws {
-        let buildType = BuildType(rawValue: buildConfiguration) ?? .debug
+        let buildType = BuildType(type: buildConfiguration) ?? .debug
         
         try NnExecutableManager.manageExecutable(buildConfiguration: buildType, at: path)
     }
